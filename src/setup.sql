b89b2e460ac7e2068e61eea54cdf27efab8e5b49
k-cross <kenneth.cross@sjsu.edu>
2015-12-05 21:00:01 -0800

    final archive

--- src/setup.sql
+++ src/setup.sql
@@ -185,3 +185,34 @@ BEGIN
 	WHERE userID = uID;
 END; //
 DELIMITER ;
+
+DROP DATABASE IF EXISTS archiveDB;
+CREATE DATABASE archiveDB;
+GRANT ALL ON archiveDB.* TO 'cs157abnb'@'localhost';
+USE archiveDB;
+
+
+CREATE TABLE reservations
+    (
+        reservationID INTEGER NOT NULL,
+        userID INTEGER,
+        tableID INTEGER,
+        orderID INTEGER,
+        people INTEGER,
+        resDate DATETIME,
+        updatedAt DATETIME
+    ) ENGINE=ARCHIVE;
+
+
+#Procedure 2: Archiving
+DROP PROCEDURE IF EXISTS UpdateArchive;
+DELIMITER //
+CREATE PROCEDURE UpdateArchive()
+BEGIN
+    INSERT INTO archiveDB.reservations 
+        SELECT * FROM reserveme.reservations
+        WHERE (TO_DAYS(NOW()) - TO_DAYS(reserveme.reservations.updatedAt) > 30);
+    DELETE FROM reserveme.reservations
+        WHERE (TO_DAYS(NOW()) - TO_DAYS(reserveme.reservations.updatedAt) > 30);
+END; //
+DELIMITER ;
